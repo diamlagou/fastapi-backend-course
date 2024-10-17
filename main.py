704665396348541cca6eea0bc17e7ceb2d73f8e3
Bubu <b11117029@gemail.yuntech.edu.tw>
2024-10-17 15:53:53 +0800

    Initalized

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/hello")
+def read_hello():
+    return '<p>efk,jwafbjkwawaf</p>'
