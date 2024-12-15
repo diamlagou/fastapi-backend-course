f5245481c8041347bbb19b962d1877d5257197a3
diamlagou <zhentatong@gmail.com>
2024-12-15 21:53:49 +0800

    update

--- alembic/env.py
+++ alembic/env.py
@@ -3,6 +3,11 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+from app.main import Base
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -18,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from .database import Base, engine
+from .routers import router
+
+#Initialize FastAPI
+app = FastAPI()
+
+#Initialize Database's Table
+Base.metadata.create_all(bind=engine)
+
+#Register Router
+app.include_router(router=router, prefix="/api", tags=["todos"])
